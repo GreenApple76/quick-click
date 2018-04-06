fa72a292690afd5acf1c55b0f7412cd4bcf34f42
Kevin <GreenApple76@users.noreply.github.com>
2018-04-06 11:58:28 -0400

    remove unused styles variable

--- src/Components/ReportClicks.js
+++ src/Components/ReportClicks.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 class ReportClicks extends Component {
     render() {
-        let styles = {
-                    }
         return (
             <div className="report-clicks-container">
                 {(this.props.seconds === 0) && this.props.clicks + " clicks  @ " + parseFloat(this.props.clicks / 5) + " clicks per second"}
